cf29622dc4dcb79f8165997c1ee4e4e087f14f44
BRK-munger <katou.kai.309@gmail.com>
2022-12-11 11:46:28 +0000

    [Add]Bootstrapの導入

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ devise_for :customers,skip:[:passwords],controllers: {
 devise_for :admin,skip:[:registrations,:passwords],controllers: {
   sessions: "admin/sessions"
 }
+
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
