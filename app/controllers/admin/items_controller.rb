2e865b45ed81b683f3eceabb0205b097b4610e06
BRK-munger <katou.kai.309@gmail.com>
2022-12-12 07:48:20 +0000

    [Add]管理者側 商品一覧作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,28 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @items = Item.page(params[:page])
+  end
+
+
+  #投稿データのストロングパラメータ
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :caption, :excluded_price, :is_status)
+  end
+
 end
