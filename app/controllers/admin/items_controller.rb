a73137ecfe16872b3f182404a098b60cfffabc4a
BRK-munger <katou.kai.309@gmail.com>
2022-12-13 01:44:28 +0000

    [Add]管理者側 商品編集ページ作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -21,9 +21,18 @@ class Admin::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
 
-
-
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_items_path(item.id)
+  end
+  
+  
   #投稿データのストロングパラメータ
   private
   def item_params
