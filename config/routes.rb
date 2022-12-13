672164516d122acd3bbe7c844d43e839f16c8704
BRK-munger <katou.kai.309@gmail.com>
2022-12-13 02:32:39 +0000

    [Add]会員側のルーティング設定　

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
 # 顧客用(public)
 # URL /customers/sign_in ...
-#不要なルーティング(passwords)を削除
 devise_for :customers,skip:[:passwords],controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
 # 管理者用(admin)
-# URL /admin/sign_in ...
-#不要なルーティング(registrations, passwords)を削除
 devise_for :admin,skip:[:registrations,:passwords],controllers: {
   sessions: "admin/sessions"
 }
@@ -21,6 +18,35 @@ devise_for :admin,skip:[:registrations,:passwords],controllers: {
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
+  end
+  
+  scope module: :public do
+    root :to =>"homes#top"
+    get "homes/about"=>'homes#about', as: 'about'
+    get "customers/unsubscribed"=>'customers#check', as: 'customers/check'
+    patch "customers/withdrawal"=>'customers#withdrawal', as: 'customers/withdrawal'
+    get "cart_items/destroy_all"=>'cart_items#destroy_all', as: 'cart_items/destroy_all'
+    get "orders/check"=>'orders#check', as: 'orders/check'
+    get "orders/complete"=>'orders#complete', as: 'orders/complete'
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/information/edit' => 'customers#edit'
+    patch 'customers/infomation' => 'customers#update'
+    resources :items, only: [:index, :show]
+     get 'items/search/:id' => 'items#search',as: 'item_search'
+    resources :cart_items, only: [:index, :update,:destroy, :create] do
+      collection do
+        delete "destroy_all" => "cart_items#destroy_all"
+      end
+    end
+    
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post "check"
+        get "complete"
+      end
+    end
+    
+    resources:addresses, only: [:index, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
