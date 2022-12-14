5fc13a8c5aa5df2d1a5ffc8bf90fc5a65b9216c8
BRK-munger <katou.kai.309@gmail.com>
2022-12-14 12:19:15 +0000

    [Add]会員側 カート内商品を空にする機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,38 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+
+  def create
+    item = CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id)
+    if item
+      item.update(quantity: item.quantity.to_i + params[:cart_item][:quantity].to_i)
+    else
+      cart_item = CartItem.new(cart_item_params)
+      cart_item.customer_id = current_customer.id
+      cart_item.save
+    end
+    redirect_to cart_items_path
+  end
+
+  def index
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal } #合計金額を算出
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    CartItem.destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :quantity)
+  end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,12 @@
 class CartItem < ApplicationRecord
+  belongs_to :item
+  belongs_to :customer
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :customer_id, :item_id, :quantity, presence:
+
+  # 小計を計算
+  def subtotal
+    item.add_tax_price * quantity
+  end
 end
