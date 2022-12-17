26cd5e9440bb62b1ccc474293dab1c17adbde8e2
BRK-munger <katou.kai.309@gmail.com>
2022-12-17 10:01:02 +0000

    [Add]会員側　注文情報入力ページ作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,22 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :order_details,dependent: :destroy
+
+  POST_CODE_REGEXP = /\A\d{7}\z/
+  validates :post_code, presence: true, format: { :with => POST_CODE_REGEXP }
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :total_payment, presence: true
+
+  def full_name
+    customer.first_name + " " + customer.last_name
+  end
+
+  enum payment_method: {credit_card: 0, transfer: 1 }
+  enum status: {payment_wait: 0,payment_confirm: 1,making: 2,shipping_preparation: 3,shipped: 4}
+
+  def subtotal
+    item.with_tax_price * quantity
+  end
+
 end
