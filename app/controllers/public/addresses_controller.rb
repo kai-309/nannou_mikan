7977d59ea6dba5f70f196dd0de1b33a9a1d38c5c
BRK-munger <katou.kai.309@gmail.com>
2022-12-17 13:10:14 +0000

    [Add]会員側　配送先編集ページ作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,13 +6,29 @@ class Public::AddressesController < ApplicationController
     @addresses = Address.where(customer_id: current_customer.id)
   end
 
-
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     @address.save
     redirect_to addresses_path
   end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+
 
    private
 
