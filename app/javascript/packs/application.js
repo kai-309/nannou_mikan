b2e715c4498851ebf232e29b7454079deb3fcde2
BRK-munger <katou.kai.309@gmail.com>
2022-12-17 06:01:58 +0000

    [Add]背景画面設定2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,8 @@ import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 
 
-//const images = require.context('../images', true)
-//const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
 Rails.start()
 Turbolinks.start()
