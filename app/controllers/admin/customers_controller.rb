50071875c2df7e4a217b538177d483e5e6bc3c01
BRK-munger <katou.kai.309@gmail.com>
2022-12-18 14:49:44 +0000

    [Add]管理者側 会員一覧ページ作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,14 @@
 class Admin::CustomersController < ApplicationController
+    before_action :authenticate_admin!
+
+  def index
+    @customers = Customer.all
+    @customer = Customer.page(params[:page]).per(10)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_kana, :first_kana, :post_code, :address, :tell, :email, :is_deleted)
+  end
 end
