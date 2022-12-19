95f747ef26b329df8bb43f6e6b2764dca45d2913
BRK-munger <katou.kai.309@gmail.com>
2022-12-19 06:26:09 +0000

    [Add]管理者側 注文履歴詳細ページ作成

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,21 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_detail.update(order_detail_params)#(making_status: params[:order_detail][:making_status])
+    if @order_detail.making_status == "under_construction"
+      @order.update(status: 2)
+    elsif @order.order_details.count == @order.order_details.where(making_status: "production_completed").count
+      @order.update(status: 3)
+    end
+    redirect_to admin_order_path(@order)
+end
+
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
 end
