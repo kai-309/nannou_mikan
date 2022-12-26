4f8bf352bb24bad1eb2a3d3fa2fb89117d638d21
BRK-munger <katou.kai.309@gmail.com>
2022-12-26 00:53:46 +0000

    [Add]管理者側 会員詳細ページレイアウト

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#管理者側のログイン情報
 Admin.create!(
   email: 'test@admin.com',
   password: 'testpass'
