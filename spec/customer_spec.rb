10b00f91e5a47e6819147e8994af5442ce4b02bd
BRK-munger <katou.kai.309@gmail.com>
2022-12-28 14:24:34 +0000

    [Add]RSpecテスト項目(顧客ログイン)

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
+  describe '顧客ログインのテスト' do
+  describe 'トップ画面のテスト' do
+    before do
+      visit root_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/'
+      end
+    end
+  end
+end
+
+
   describe '会員新規登録のテスト' do
     before do
       visit new_customer_registration_path
